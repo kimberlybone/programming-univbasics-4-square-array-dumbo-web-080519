433a5120d82f098976eb068ee450d667be3f4322
Kimberly Bone <k_bone@uncg.edu>
2019-06-17 20:05:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,6 +4,6 @@ def square_array(array)
   new_array = []
   while counter < array.length do 
     counter += 1 
-    new_array.push (array[counter ** 2)
+    new_array.push (array[counter] ** 2)
   end 
 end
